ab0f3d5152e09136aa0e1eb8482f702a93be44f2
Jimmy <xxg1413@gmail.com>
2012-11-13 09:43:12 +0800

    new file:   beginning_python/code/3_1.py 	new file:   beginning_python/pdf/chapter3.pdf

---
+++ beginning_python/code/3_1.py
@@ -0,0 +1,23 @@
+﻿#XXG 2012-11-13
+
+#使用给定的宽度打印格式化后的价格列表
+
+width = input("please enter width:") #使用raw_input更安全
+
+price_width = 10
+item_width = width - price_width
+
+header_format = '%-*s%*s'
+format        = '%-*s%*.2f'
+
+print '=' * width
+
+print header_format % (item_width,'Item',price_width,'Pice')
+
+print '-' * width
+
+print format % (item_width,'Apples',price_width,0.4)
+print format % (item_width,'Pears',price_width,0.5)
+
+
+print '=' * width
