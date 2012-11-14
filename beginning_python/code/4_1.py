8a4ea6c12a3283aea3b6bd86b3eb3b72c7ebb29d
Jimmy <xxg1413@gmail.com>
2012-11-14 20:02:46 +0800

    new file:   beginning_python/code/4_1.py 	new file:   beginning_python/code/4_2.py 	new file:   beginning_python/pdf/chapter4.pdf

---
+++ beginning_python/code/4_1.py
@@ -0,0 +1,32 @@
+﻿# XXG 2012-11-14
+
+#简单数据库
+people ={
+    'Alice':{
+        'phone':'123',
+        'addr':'abc'
+
+        },
+    'Jimmy':{
+        'phone':'456',
+        'addr':'def'
+        }
+    }
+
+#针对电话号码和地址使用描述标签
+labels={
+    'phone':u'电话号码',
+    'addr':u'地址'
+    }
+
+info    = u"请输入你要查询的分类 电话号码(p) 地址(a)"
+name    = raw_input(u"请输入姓名：")
+request = raw_input(info)
+
+if request=='p': key='phone'
+if request=='a': key='addr'
+
+if name in people:
+    print u"%s %s 是 %s" % (name,labels[key],people[name][key])
+
+#？全部输出
---
+++ beginning_python/code/4_2.py
@@ -0,0 +1,20 @@
+﻿# XXG 2012-11-14
+
+#使用get的简单数据库
+
+people = {
+    'Alice':{
+        'phone':'123',
+        'addr':'abc'
+        }
+    }
+
+labels = {
+    'phone': u'电话号码',
+    'addr': u'地址'
+    }
+
+name = raw_input("请输入姓名：")
+
+print  u"%s的%s是%s\n   %s 是 %s " % (name,labels.get('phone'),people.get(name).get('phone'),labels['addr'],people[name].get('addr'))
+
